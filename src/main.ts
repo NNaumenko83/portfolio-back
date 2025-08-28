ffd8e3615abf075d69f0eb13d12e0ff18918bcf4
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2025-08-28 15:50:20 +0300

    feat: enhance API documentation with global prefix and add ApiTags to OwnerController

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Portfolio API')
     .setDescription('The Portfolio API description')
     .setVersion('1.0.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
   SwaggerModule.setup('api/docs', app, document);
-  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
--- src/owner/owner.controller.ts
+++ src/owner/owner.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { OwnerService } from './owner.service';
 import { GetOwnerDto } from './dto/get-owner.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 
-
+@ApiTags('Owner')
 @Controller('owner')
 export class OwnerController {
   constructor(private readonly ownerService: OwnerService) { }
