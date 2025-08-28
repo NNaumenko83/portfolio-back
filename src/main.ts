570e633f60bf67c80b509f62dc6914ed18bbfc35
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2025-08-28 15:37:51 +0300

    feat: add Swagger integration for API documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Portfolio API')
+    .setDescription('The Portfolio API description')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
