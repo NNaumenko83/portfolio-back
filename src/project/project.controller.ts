66244a47540d58b3d9a4b169220d2025ef76fcf5
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2025-08-28 15:20:06 +0300

    feat: implement project controller and service with CRUD operations

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProjectService } from './project.service';
+import { Project } from '@prisma/client';
 
-@Controller('project')
+@Controller('projects')
 export class ProjectController {
-  constructor(private readonly projectService: ProjectService) {}
+  constructor(private readonly projectService: ProjectService) {
+
+  }
+  @Get()
+  async getProjects(): Promise<Project[]> {
+    return this.projectService.getProjects();
+  }
+
+  @Get(':id')
+  async getProjectById(@Param('id') id: string): Promise<Project | null> {
+    return this.projectService.getProjectById(id);
+  }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Project } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getProjects(): Promise<Project[]> {
+        return this.prisma.project.findMany(
+            {
+                include: {
+                    techStacks: true,
+                }
+            },
+
+        );
+    }
+
+    async getProjectById(id: string): Promise<Project | null> {
+        return this.prisma.project.findUnique({
+            where: { id },
+            include: {
+                techStacks: true,
+            },
+        });
+    }
+}
